c23824af00090b61e04f61ec4fc8459e41a1a592
Kenwoll <i.murad.2003@gmail.com>
2024-03-31 04:08:34 +0400

    initial nvim setup

---
+++ lua/vim-options.lua
@@ -0,0 +1,8 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.g.mapleader = " "
+vim.o.number = true
+vim.g.netrw_list_hide = '^\\.,\\(.*\\)\\~$'
+
